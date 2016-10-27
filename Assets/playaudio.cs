9f46a4dddc82e7c50b5a7d27a6e2da3dbd3a6108
Richard Taylor <rsjtaylor.github@gmail.com>
2016-10-27 15:44:19 +0100

    Soundfield test project

---
+++ Assets/playaudio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playaudio : MonoBehaviour {
+	private GvrAudioSoundfield soundfield;
+	private bool started = false;
+	// Use this for initialization
+	void Start () {
+		soundfield = GetComponent<GvrAudioSoundfield> ();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (!started) {
+			soundfield.Play ();
+			started = true;
+		}
+	}
+}
